b54955bc3d8b8a6646d9ea7448d064ecd1a32a9c
syakovlev <fugbydColt5>
2018-03-02 15:04:55 +0300

    join left group

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '554973825:AAHmU_hSs5OYsmE9iQkbRW_JsjUSGIJ6LDE';
-const site = 'http://www.localhost:8000';
+const site = 'http://127.0.0.1:8000';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
@@ -11,19 +12,38 @@ bot.onText(/\/start (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
+  
+
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const resp = match[1]; 
   let html = '';   
-  // send back the matched "whatever" to the chat
-  if(resp.length>0){
-  html = `
-    Click here (<a href='${site}/invite?tg=${resp}'>Link</a>) to register for a free account on Token
-  `;
-  }
-  console.log(html);
-  bot.sendMessage(chatId, html,{parse_mode:'HTML'});
+
+    
+    const userId = msg.from.id;
+    if(resp.length>0){
+        html = `Group <a href='https://t.me/joinchat/CUgtSA92tRbkE_KP1Lgpvw'> Link </a>\n`;
+        html +=`Click here (<a href='${site}/invite?tg=${resp}&telegram_id=${userId}'>Link</a>) to register for a free account on Token`
+        }
+        console.log(html);
+        bot.sendMessage(chatId, html,{parse_mode:'HTML'});
+  
 });
+
+bot.on('new_chat_members', (msg,match) => {
+    console.log('new_chat_members',msg,match);
+    console.log(`${site}/telegramjoin?telegram_id=${msg.from.id}`);
+    request.get(`${site}/telegramjoin?telegram_id=${msg.from.id}`)
+})
+
+bot.on('left_chat_member', (msg) => {
+    console.log('left_chat_member');
+    request.get(`${site}/telegramleft?telegram_id=${msg.from.id}`)
+})
+
+// bot.onText(/.*/, (msg, match) => {
+//     console.log(msg,match);
+// })
 
 // Listen for any kind of message. There are different kinds of
 // messages.
